f83793199c050bcf312a233d2b92b3524a1b783a
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 19:32:35 +0300

    Create booleanOr.java

---
+++ booleanOr.java
@@ -0,0 +1,20 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//BOOLEAN OPERATORS
+		
+		double e = 2.3;
+		double f = 2.4;
+		double g = 2.5;
+		
+		 
+		 // || (or) ONE condition must be true => True	
+		 if (f > e || f > g) {
+			 System.out.println("One condition is true, in order for you to see this message.");
+		 }
+	
+    
+	}
+}
