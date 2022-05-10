bce7023a958565937c02817c20371b73a3606d84
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 20:06:48 +0300

    Create for.java

---
+++ for.java
@@ -0,0 +1,13 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//for loop
+	
+		for (int i = 0; i < 10; i=i+2) {
+			System.out.println("i: " + i); //prints all even values between 0 and 10; exits loop at 8; 8 < 10
+		}
+	
+	}
+}
