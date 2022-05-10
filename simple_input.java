957f0ca4fb228e27e5d612ff6dae3751291e470e
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 21:34:34 +0300

    Create simple_input.java

---
+++ simple_input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//User Input
+	
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter a number: ");
+		int myInt = scan.nextInt(); //-> gets next input as int
+		System.out.println("Your number is: " + myInt);
+		
+	
+	}
+}
