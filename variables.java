8014afb4d0c0739b89e319589bdd8ebb4850fb6e
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 17:39:30 +0300

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,26 @@
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		//Printing using SOPL = System.out.println()
+		System.out.println("Hello Circle!");
+		System.out.println(); //printeaza rand gol
+		
+		
+		/*
+		 * Definirea variabilelor
+		 */
+		
+		
+		int x = 5;
+		double y = 5.0;
+		boolean flag = true;
+		
+    
+    //concatenare
+		System.out.println("x: " + x);
+		System.out.println("y: " + y);
+		System.out.println("flag: " + flag);
+	}
+
+}
