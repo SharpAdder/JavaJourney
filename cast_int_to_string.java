8fe6deca0908cb4baac87fc507f4e3ae8b840778
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 20:32:53 +0300

    Create cast_int_to_string.java

---
+++ cast_int_to_string.java
@@ -0,0 +1,19 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//CASTING
+	
+		//cast int to string
+		String intToString = Integer.toString(1);
+		
+		//Cast double to String
+		String doubleToString = Double.toString(1.1);
+		
+		//printing values and their data type
+		System.out.println(intToString + " " + intToString.getClass());
+		System.out.println(doubleToString + " " + doubleToString.getClass());
+	
+	}
+}
