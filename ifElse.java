08d9fd6de5335ef58f181f5b801c38c18cbd6567
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 19:08:07 +0300

    Create ifElse.java

---
+++ ifElse.java
@@ -0,0 +1,19 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//CONDITIONALS
+		 int x = 5;
+		 
+		 System.out.println("x: " + x);
+		 
+     //if else
+		 if (x % 2 == 0) {
+			 System.out.println(x + " is even");
+		 } else {
+			 System.out.println(x + " is odd");
+		 }
+		
+	}
+}
