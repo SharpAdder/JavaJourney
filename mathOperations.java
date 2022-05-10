21e75cd4ac593bd1c30a9b0c2d4f57e6fede74e6
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 18:45:21 +0300

    Create mathOperations.java

---
+++ mathOperations.java
@@ -0,0 +1,25 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//MATH OPERATIONS
+		
+		int x = 5;
+		double y = 5.5;
+		double d = 2 * x + 10;
+		double z = 2 * y + 5;
+		
+		System.out.println("d = " + d);
+		System.out.println("z = " + z);
+		
+		//division with ints -> drop remainder
+		System.out.println("x/2 = " + (x/2));
+		
+		//division with floats
+		System.out.println("x/2.0 = " + (x/2.0));
+		
+		//power operations
+		System.out.println("x pow 4 = " + Math.pow(x, 4));
+	}
+}
