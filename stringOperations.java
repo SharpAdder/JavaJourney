9c898771dca6c734cb6677d57cf60f7a1188e96a
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 18:56:42 +0300

    Create stringOperations.java

---
+++ stringOperations.java
@@ -0,0 +1,17 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//STRING OPERATIONS
+		
+		//String concatenation
+		String fullName = "Adina" + " " + "Adam";
+		
+		//method toUppercase -> converts string
+		String fullNameUpper = fullName.toUpperCase();
+		
+		System.out.println(fullNameUpper);
+		
+	}
+}
