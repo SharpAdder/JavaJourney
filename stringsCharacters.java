a4d21e822b1a4bfc03935c8f494eace2ad128417
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 18:21:51 +0300

    Create stringsCharacters.java

---
+++ stringsCharacters.java
@@ -0,0 +1,17 @@
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//STRINGS & CHARACTERS
+		String course = "Java ";
+		char letter = 'A';
+		
+		String level = course + 101;
+		String grade = letter + "";
+		
+		String courseInformation = level + ": " + grade;
+		System.out.println(courseInformation);
+		
+	}
+
+}
