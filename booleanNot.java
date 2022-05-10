25854f90b9170e4e0127b2151318b8b7c5a46076
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 19:39:02 +0300

    Create booleanNot.java

---
+++ booleanNot.java
@@ -0,0 +1,18 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//BOOLEAN OPERATORS
+		
+		double e = 2.3;
+		double f = 2.4;
+		double g = 2.5;
+		
+		// ! (not) = negation; !true => false
+		if (g != 2.6) {
+			System.out.println(g + " is not equal to 2.6");
+		}
+	
+	}
+}
