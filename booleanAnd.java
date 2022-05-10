97ed34a7613a4d836cff2ce72042dde3b3490bf8
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-05-10 19:22:12 +0300

    Create booleanAnd.java

---
+++ booleanAnd.java
@@ -0,0 +1,18 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		//BOOLEAN OPERATORS
+		
+		double e = 2.3;
+		double f = 2.4;
+		double g = 2.5;
+		
+		// && (and) both conditions must be true => True
+		 
+		 if (e > 2 && e < f) {
+			 System.out.println(e + " is between 2 and " + f);
+		 }
+	}
+}
